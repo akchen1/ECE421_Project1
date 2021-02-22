f977047bc7fe3f42a874308a50daea69565ddd36
Ming <44931428+mingyaang@users.noreply.github.com>
2021-02-21 16:59:05 -0800

    Update PickShareFunctional.java

--- src/PickShareFunctional.java
+++ src/PickShareFunctional.java
@@ -6,16 +6,16 @@ import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class PickShareFunctional {
-    static ShareInfo findHighPrices(Stream<String> shares) {
-        final Predicate isPriceLessThan500 = ShareUtil.isPriceLessThan(500);
+    static Optional<ShareInfo> findHighPrices(Stream<String> shares) {
+        final Predicate<? super ShareInfo> isPriceLessThan500 = ShareUtil.isPriceLessThan(500);
 
         AtomicInteger index = new AtomicInteger();
 
-        Optional result = shares.map(symbol -> {
+        return shares.map(symbol -> {
             if (index.get() != 0 && index.get() % 5 == 0) {
                 try {
+                    System.out.println("Sleeping thread for limiter ...");
                     Thread.sleep(60000);
-                    System.out.println("Finished");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -27,27 +27,31 @@ public class PickShareFunctional {
             System.out.println(price);
 
             return price;
-        }).filter(isPriceLessThan500).max(Comparator.comparing(s -> ((ShareInfo)s).price));
-
-        return (ShareInfo) result.get();
+        }).filter(isPriceLessThan500).max(Comparator.comparing(s -> s.price));
     }
 
     public static void main(String[] args) {
 
         long startTime = System.nanoTime();
-        ShareInfo max = PickShareFunctional.findHighPrices(Shares.symbols.parallelStream());
+        Optional<ShareInfo> max = PickShareFunctional.findHighPrices(Shares.symbols.parallelStream());
         long stopTime = System.nanoTime();
-        System.out.println((stopTime - startTime) / 1000000 + "Milliseconds");
+        System.out.println((stopTime - startTime) / 1000000 + " Milliseconds");
 
-        System.out.println("High priced under $500 is " + max);
+        if (max.isPresent())
+            System.out.println("High priced under $500 is " + max.get());
+        else
+            System.out.println("None high priced under $500");
+
 
         long startTime2 = System.nanoTime();
-        ShareInfo max2 = PickShareFunctional.findHighPrices(Shares.symbols.stream());
+        Optional<ShareInfo> max2 = PickShareFunctional.findHighPrices(Shares.symbols.stream());
         long stopTime2 = System.nanoTime();
         System.out.println((stopTime2 - startTime2) / 1000000 + "Milliseconds");
 
-        System.out.println("High priced under $500 is " + max);
-
+        if (max2.isPresent())
+            System.out.println("High priced under $500 is " + max2.get());
+        else
+            System.out.println("None high priced under $500");
 //        long startTime = System.nanoTime();
 //        final Predicate<ShareInfo> isPriceLessThan500 = ShareUtil.isPriceLessThan(500);
 //        ShareInfo highPriced = new ShareInfo("", new BigDecimal(-1));
